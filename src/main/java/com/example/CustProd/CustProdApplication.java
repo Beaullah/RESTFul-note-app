5680cfc1c8892d392d2545b9d41a5cf9d6aeab0b
Redeemm <71359434+Redeemm@users.noreply.github.com>
2022-06-23 15:46:41 +0000

    .

---
+++ src/main/java/com/example/CustProd/CustProdApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustProd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustProdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CustProd/CustProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustProd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
